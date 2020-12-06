e66a2fffb31ff51419c2db26ca6894f0c98d3f3c
sejalv <sejalv@gmail.com>
2020-12-06 17:17:06 +0100

    model to ecr image

--- service/app.py
+++ service/app.py
@@ -5,9 +5,9 @@ import pickle
 import sklearn
 from sklearn.linear_model import LogisticRegression
 
-model_bucket = "serverless-ml-workshop-modelbucket-11be8460mjrb1"
-model_key = "pickled_model.p"    # env
-model_local_path = "/tmp/pickled_model.p"
+# model_bucket = "serverless-ml-workshop-modelbucket-11be8460mjrb1"
+# model_key = "pickled_model.p"    # env
+model_local_path = "model/pickled_model.p"
 
 
 def lambda_handler(event, context):
@@ -18,15 +18,14 @@ def lambda_handler(event, context):
     #     print(s3_client.head_object(Bucket=model_bucket, Key=model_key))
     #     s3_client.download_file(model_bucket, model_key, model_local_path)
 
-    s3 = boto3.resource('s3')
-    s3.Bucket(model_bucket).download_file(model_key, model_local_path)
-
-    body = event['body']
-    input = json.loads(body)['data']
-    input = float(input)
-
     with open(model_local_path, 'rb') as f:
         model = pickle.load(f)
+
+    body = event['body']
+    # input = json.loads(body)['data']
+    data = body['data']
+    input = float(data)
+
     # Predict class
     prediction = model.predict([[input]])[0]
 
--- training/train.py
+++ training/train.py
@@ -5,22 +5,23 @@ import numpy as np
 import pickle
 import os
 
-MODEL_DIR = os.environ["MODEL_DIR"]
-MODEL_FILE = os.environ["MODEL_FILE"]
-METADATA_FILE = os.environ["METADATA_FILE"]
-MODEL_PATH = os.path.join(MODEL_DIR, MODEL_FILE)
-METADATA_PATH = os.path.join(MODEL_DIR, METADATA_FILE)
+# MODEL_DIR = os.environ["MODEL_DIR"]
+# MODEL_FILE = os.environ["MODEL_FILE"]
+# METADATA_FILE = os.environ["METADATA_FILE"]
+# MODEL_PATH = os.path.join(MODEL_DIR, MODEL_FILE)
+# METADATA_PATH = os.path.join(MODEL_DIR, METADATA_FILE)
 
-data = load_wine() # import dataset
-df = pd.DataFrame(data['data'], columns=data['feature_names']) # build dataframe
-df['target'] = data['target'] # add dependent variable
-df = df.sample(frac=1) # randomize the data
+data = load_wine()  # import dataset
+df = pd.DataFrame(data['data'], columns=data['feature_names'])  # build dataframe
+df['target'] = data['target']   # add dependent variable
+df = df.sample(frac=1)  # randomize the data
 df.head(3)
 
 print("row count:",len(df))
 train_df = df[:150]
 test_df = df[150:]
 #=> row count: 178
+
 
 def X_and_y_from_df(df, y_column, X_columns = []):
     '''Extract data from the dataframe'''
@@ -29,6 +30,7 @@ def X_and_y_from_df(df, y_column, X_columns = []):
         X[feature] = df[feature].tolist()
     y = df[y_column].tolist()
     return X, y
+
 X_train, y_train = X_and_y_from_df(train_df, 'target', ['alcohol'])
 X_test, y_test = X_and_y_from_df(test_df, 'target', ['alcohol'])
 X_train = np.array(X_train['alcohol']).reshape(-1,1)
@@ -37,4 +39,4 @@ X_test = np.array(X_test['alcohol']).reshape(-1,1)
 model = LogisticRegression()
 model.fit(X_train, y_train)
 
-pickle.dump( model, open( MODEL_DIR+"/pickled_model.p", "wb" ) )
+pickle.dump( model, open("model/pickled_model.p", "wb" ) )
